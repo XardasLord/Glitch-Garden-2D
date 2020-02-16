99221f968021ad7d28d059b40c1247d7c22b5f7a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-02-16 15:06:42 +0100

    Dealing damage & health mechanism

---
+++ Assets/Scripts/Attackers/AttackerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Attackers
+{
+    public class AttackerHealth : MonoBehaviour
+    {
+        [SerializeField] private int health = 100;
+
+        private void Awake()
+        {
+        }
+
+        public void DealDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Defenders/ProjectileDamage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Assets.Scripts.Attackers;
+using UnityEngine;
+
+namespace Defenders
+{
+    public class ProjectileDamage : MonoBehaviour
+    {
+        [SerializeField] private int damageValue = 100;
+
+        public event Action<int> OnDealDamage = delegate { };
+
+        public int Damage { get; private set; }
+
+        private void Awake()
+        {
+            Damage = damageValue;
+        }
+
+        private void OnTriggerEnter2D(Collider2D otherCollider)
+        {
+            //TODO: Fire an event!
+            //OnDealDamage(Damage);
+
+            otherCollider.GetComponent<AttackerHealth>().DealDamage(Damage);
+        }
+    }
+}
