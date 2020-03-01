d15627db8d5ef0187c5ded1e9a90f3ae59454524
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-01 09:09:29 +0100

    Spawning defender cactus on mouse click

---
+++ Assets/Scripts/Defenders/DefenderSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Defenders
+{
+    public class DefenderSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject defender;
+
+        public event Action DefenderSpawned = delegate { };
+
+        private void OnMouseDown()
+        {
+            SpawnDefender();
+        }
+
+        private void SpawnDefender()
+        {
+            var newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+            DefenderSpawned();
+        }
+    }
+}
