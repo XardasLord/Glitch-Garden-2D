9ee66d62627a1f8090b49cd47ac3f1e3c537ef3b
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-01 09:17:42 +0100

    Spawning defender at mouse position

--- Assets/Scripts/Defenders/DefenderSpawner.cs
+++ Assets/Scripts/Defenders/DefenderSpawner.cs
@@ -11,12 +11,20 @@ namespace Defenders
 
         private void OnMouseDown()
         {
-            SpawnDefender();
+            SpawnDefender(GetSquareClicked());
         }
 
-        private void SpawnDefender()
+        private static Vector2 GetSquareClicked()
         {
-            var newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+            var clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            var worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+            return worldPos;
+        }
+
+        private void SpawnDefender(Vector2 worldPos)
+        {
+            Instantiate(defender, worldPos, Quaternion.identity);
             DefenderSpawned();
         }
     }
