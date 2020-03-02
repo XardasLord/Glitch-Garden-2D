8dbb8e3ea25ab18132c931d7c8adee90e144d83e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-02 20:05:00 +0100

    Menu UI for selecting defenders

---
+++ Assets/Scripts/UI/DefenderMenuSelection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class DefenderMenuSelection : MonoBehaviour
+    {
+        private SpriteRenderer _spriteRenderer;
+        private DefenderMenuSelection[] _defenderMenuSelections;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _defenderMenuSelections = FindObjectsOfType<DefenderMenuSelection>();
+        }
+
+        private void OnMouseDown()
+        {
+            foreach (var defenderMenu in _defenderMenuSelections)
+            {
+                defenderMenu.GetComponent<SpriteRenderer>().color = new Color32(79, 79, 79, 255);
+            }
+
+            _spriteRenderer.color = Color.white;
+        }
+    }
+}
