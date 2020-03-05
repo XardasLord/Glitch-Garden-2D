b38ba521749ca591ec90ea5f45dc4ebda8592ff8
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-05 16:51:38 +0100

    Added stars score to the UI

---
+++ Assets/Scripts/UI/StarDisplay.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    public class StarDisplay : MonoBehaviour
+    {
+        [SerializeField] private int stars = 100;
+
+        private Text _starText;
+
+        private void Start()
+        {
+            _starText = GetComponent<Text>();
+            UpdateDisplay();
+        }
+
+        public void AddStar(int amount)
+        {
+            stars += amount;
+            UpdateDisplay();
+        }
+
+        public void SpendStars(int amount)
+        {
+            if (stars >= amount)
+            {
+                stars -= amount;
+                UpdateDisplay();
+            }
+        }
+
+        private void UpdateDisplay()
+        {
+            _starText.text = stars.ToString();
+        }
+    }
+}
