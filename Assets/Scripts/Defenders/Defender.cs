46d6dc2df91a283a49bc98e807967606f846e0f8
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-03-08 21:04:06 +0100

    Adding stars as resources mechanism

--- Assets/Scripts/Defenders/Defender.cs
+++ Assets/Scripts/Defenders/Defender.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using UI;
+using UnityEngine;
 
 namespace Defenders
 {
     public class Defender : MonoBehaviour
     {
         [SerializeField] private int starCost = 100;
+
+        public void AddStars(int amount)
+        {
+            FindObjectOfType<StarDisplay>().AddStar(amount);
+        }
     }
 }
