783215b08f46b2e65771148dd5feedc7936ae27e
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-05-17 12:15:12 +0200

    Fox behaviour

---
+++ Assets/Scripts/Defenders/Gravestone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Attackers;
+using UnityEngine;
+
+namespace Defenders
+{
+    public class Gravestone : MonoBehaviour
+    {
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            var attacker = other.GetComponent<Attacker>();
+
+            if (attacker)
+            {
+                // TODO: Add some sort of animation
+            }
+        }
+    }
+}
