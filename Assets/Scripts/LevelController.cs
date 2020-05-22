c4b31a2208d82cd40886855d288d6b296beceabc
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-05-22 09:35:40 +0200

    Level completing mechanism

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,10 +1,14 @@
-﻿using Attackers;
+﻿using System.Collections;
+using Assets.Scripts;
+using Attackers;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] private GameObject winLabel;
+
     private bool _gameTimerExpired;
-    private int _numberOfAttackers = 0;
+    private int _numberOfAttackers;
 
     private void Start()
     {
@@ -13,6 +17,8 @@ public class LevelController : MonoBehaviour
         {
             attackerSpawner.OnSpawned += AttackerSpawner_OnSpawned;
         }
+
+        winLabel.SetActive(false);
     }
 
     private void GameTimerExpired()
@@ -38,7 +44,17 @@ public class LevelController : MonoBehaviour
 
         if (_numberOfAttackers <= 0 && _gameTimerExpired)
         {
-            Debug.Log("End level now!");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    private IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+
+        yield return new WaitForSeconds(4f);
+
+        FindObjectOfType<LevelLoader>().LoadNextScene();
     }
 }
